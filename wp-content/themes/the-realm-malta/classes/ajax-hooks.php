92c2d24231c71b25f6a49aaa059c77cecba0cd76
James Borg <james.borg@raketech.com>
2026-01-11 16:07:50 +0100

    Added hooks class

--- wp-content/themes/the-realm-malta/classes/ajax-hooks.php
+++ wp-content/themes/the-realm-malta/classes/ajax-hooks.php
@@ -1,1 +1,14 @@
 <?php
+
+class TRM_AJAX_Hooks extends TRM_Core
+{
+    public function __construct()
+    {
+        $this->register_hook_callbacks();
+    }
+    
+    public function register_hook_callbacks()
+    {
+        
+    }
+}
